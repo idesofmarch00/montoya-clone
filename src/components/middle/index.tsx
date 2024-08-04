f35b6fe7dbbeed35034e8f5d2a92ade677501b39
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-08-05 00:20:35 +0530

    text changes middle component

--- src/components/middle/index.tsx
+++ src/components/middle/index.tsx
@@ -5,13 +5,14 @@ import Magnetic from "../magnetic";
 const Middle = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <p className="text-lg font-mono max-w-2xl text-wrap text-gray-400">
-        WE ARE A CREATIVE STUDIO, SPECIALIZED IN STRATEGY, BRANDING DESIGN, AND
-        DEVELOPMENT. OUR WORK IS ALWAYS AT THE INTERSECTION OF DESIGN AND
-        TECHNOLOGY.
+      <p className="text-lg font-mono max-w-lg text-wrap text-gray-400 pt-10 text-center">
+        CRAFTING WEBSITES WHERE THE ELEGANCE
+      </p>
+      <p className="text-lg font-mono max-w-2xl text-wrap text-gray-400 pb-10 text-center">
+        OF DESIGN INTERSECTS WITH THE SCIENCE OF SELLING PRODUCTS.
       </p>
       <Magnetic>
-        <div className="border-2 border-white rounded-2xl min-w-40 h-10">
+        <div className="border-2 border-white rounded-2xl min-w-40 h-10 px-3 pt-2">
           <motion.div
             initial="initial"
             whileHover="hovered"
