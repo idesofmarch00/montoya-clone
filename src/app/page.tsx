e451a717a26d2493a4d2f6d6b4ef9e09ea7ab2cc
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-08-04 23:17:22 +0530

    init cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useRef } from "react";
 import Header from "../components/header";
 import StickyCursor from "../components/stickyCursor";
 import Hero from "../components/hero";
+import Projects from "@/components/projects";
 
 export default function Home() {
   const stickyElement = useRef(null);
@@ -11,6 +12,7 @@ export default function Home() {
     <main className="bg-black h-screen">
       <Header ref={stickyElement} />
       <Hero />
+      <Projects />
       <StickyCursor stickyElement={stickyElement} />
     </main>
   );
