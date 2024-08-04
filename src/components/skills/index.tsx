1f9da0dc931268dd3e67ea300dc7f8132a231e79
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-08-05 00:16:15 +0530

    some ui changes

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,17 +1,37 @@
 "use client";
-import { useRef } from "react";
-import { useScroll } from "framer-motion";
+import { useRef, useEffect } from "react";
+import { useScroll, motion } from "framer-motion";
 
 const Skills = () => {
-  const container = useRef(null);
+  const container = useRef<HTMLDivElement | null>(null); // Specify the type for the ref
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ["start start", "end end"],
   });
 
+  useEffect(() => {
+    if (scrollYProgress.get() > 0.95) {
+      // Use get() method instead of current
+      if (container.current) {
+        // Check if container.current is not null
+        window.scrollTo({
+          top: container.current.offsetTop + 200, // Adjust the offset as needed
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [scrollYProgress]);
+
   return (
-    <div ref={container}>
-      <p className="text-4xl font-bold sticky top-0">Skills</p>
+    <div ref={container} className="flex flex-col items-center pt-40">
+      <motion.p
+        className="text-xs font-bold sticky top-10"
+        style={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        OUR SKILLS COVER
+      </motion.p>
       <div className="flex flex-col items-center justify-center h-screen">
         <ul>
           <li className="text-8xl font-bold">Skill 1</li>
