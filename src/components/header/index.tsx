e524ec258b47d945e05381f335e493986cee09e0
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-08-04 21:30:26 +0530

    hamburger and menu on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,25 +1,52 @@
 import { forwardRef } from "react";
+import { motion } from "framer-motion";
+
 import Magnetic from "../magnetic";
 
-const Header = forwardRef<HTMLDivElement, any>(function index(props, ref) {
+const Header = forwardRef(function Header(props, ref) {
+  const lineVariants = {
+    initial: { width: 0 },
+    hover: { width: "100%" },
+  };
+
+  const textVariants = {
+    initial: { y: 20, opacity: 0 },
+    hover: { y: 0, opacity: 1 },
+  };
+
   return (
-    <div className="fixed flex w-full justify-end p-2 box-border cursor-pointer mix-blend-difference z-10">
-      <style jsx>{`
-        .burger::before,
-        .burger::after {
-          content: "";
-          display: block;
-          width: 30px;
-          height: 2px;
-          mix-blend-mode: difference;
-          background-color: white;
-        }
-      `}</style>
+    <div className="fixed top-0 right-0 flex items-center w-full justify-end box-border cursor-pointer mix-blend-difference z-10">
+      <motion.div
+        initial="initial"
+        whileHover="hovered"
+        className="h-fit relative overflow-hidden font-black uppercase cursor-pointer"
+      >
+        <motion.div
+          variants={{
+            initial: { y: 0 },
+            hovered: { y: -30 },
+          }}
+        >
+          Menu
+        </motion.div>
+        <motion.div
+          className="absolute inset-0"
+          variants={{
+            initial: { y: 30, opacity: 0 },
+            hovered: { y: 0, opacity: 1 },
+          }}
+        >
+          Menu
+        </motion.div>
+      </motion.div>
+
       <Magnetic>
-        <div className="burger relative flex flex-col gap-2 p-8 pointer-events-none">
+        <div className="relative flex flex-col gap-2 p-7.5 pointer-events-none">
+          <div className="w-7.5 h-0.5 bg-white mix-blend-difference"></div>
+          <div className="w-7.5 h-0.5 bg-white mix-blend-difference"></div>
           <div
             ref={ref as React.Ref<HTMLDivElement>}
-            className="absolute left-0 top-0 w-40 h-40 pointer-events-all hover:transform"
+            className=" absolute left-0 top-0 w-full h-full pointer-events-auto hover:scale-300 transition-transform rounded-full"
           ></div>
         </div>
       </Magnetic>
