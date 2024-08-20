ae9f944ec1d6985cd711a712b55daa7ef580e43b
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-08-20 21:23:42 +0530

    studio letters fixed

--- src/components/studio/index.tsx
+++ src/components/studio/index.tsx
@@ -24,7 +24,7 @@ const Studio = () => {
   return (
     <div className="flex pt-20 flex-col items-center justify-center h-screen text-white text-center z-0">
       <p className="text-lg max-w-2xl text-wrap text-gray-400">
-        EXPLORING OUR WORLD OF VISUAL AND INTERACTIVE DESIGN
+        EXPLORING OUR WORLD OF VISUAL AND INTERACTIVE DESI`GN
       </p>
       <div className="flex items-center space-x-4">
         <motion.h1 className="text-[18.5rem] font-medium flex">
@@ -33,12 +33,11 @@ const Studio = () => {
               key={index}
               className="inline-block relative"
               initial={{ scaleY: 1 }}
-              whileHover={{ scaleY: 1.1 }}
-              transition={{ type: "spring", stiffness: 100, damping: 5 }}
+              whileHover={{ scaleY: 1.3, y: -10 }}
+              transition={{ duration: 0.5, type: "spring" }}
               style={{
                 transformOrigin: "top center",
                 display: "inline-block",
-                position: "relative",
                 overflow: "hidden",
                 lineHeight: 1,
               }}
@@ -56,12 +55,14 @@ const Studio = () => {
               key={index}
               className="inline-block relative"
               initial={{ scaleY: 1 }}
-              whileHover={{ scaleY: 1.1 }}
-              transition={{ type: "spring", stiffness: 100, damping: 5 }}
+              whileHover={{ scaleY: 1.3, y: -10 }}
+              transition={{
+                duration: 0.5,
+                type: "spring",
+              }}
               style={{
                 transformOrigin: "top center",
                 display: "inline-block",
-                position: "relative",
                 overflow: "hidden",
                 lineHeight: 1,
               }}
