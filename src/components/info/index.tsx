3219229dea536b8490825166d29b8bc4d5859d6b
Sahil Ahmed <sahilahmed@Sahils-MacBook-Pro.local>
2024-08-23 03:29:07 +0530

    responsive info md

--- src/components/info/index.tsx
+++ src/components/info/index.tsx
@@ -4,7 +4,7 @@ import { infoData } from "@/data/infoData";
 // Component to render the info section
 const Info = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center md:px-20 md:text-lg -mt-30 sm:-mt-20 md:mt-10">
+    <div className="flex flex-col md:flex-row md:text-sm justify-between items-center md:px-20 lg:text-lg -mt-40 sm:-mt-20 md:mt-10">
       {infoData.map((item, index) => (
         <div key={index} className="p-4 rounded-lg shadow-md m-4">
           <div className="flex flex-col items-center justify-center space-y-2">
